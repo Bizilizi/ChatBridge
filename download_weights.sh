3fc0b2262ad8674c51f9add752aaf34f91bb3643
Daniil Zverev <zverev@in.tum.de>
2025-05-05 21:58:47 +0200

    Add script to download BEATs model weights and create directory structure for organization

--- download_weights.sh
+++ download_weights.sh
@@ -1,6 +1,9 @@
 mkdir -p vicuna_weights/13b/
-
 huggingface-cli download Vision-CAIR/vicuna --local-dir vicuna_weights/13b/ --local-dir-use-symlinks False
+
+mkdir beats_weights
+cd beats_weights
+wget https://huggingface.co/nsivaku/nithin_checkpoints/resolve/main/BEATs_iter3_plus_AS2M.pt?download=true -O BEATs_iter3_plus_AS2M.pt
 
 
 
